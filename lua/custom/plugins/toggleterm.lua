51be09c24f34fe731bb7ac6bfa70dede2533dd38
Thomas Peterson <info@thomas-peterson.de>
2025-01-05 19:40:55 +0100

    ToggleTerm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -13,11 +13,24 @@ return {
         hidden = true,
       }
 
+      local normalterm = Terminal:new {
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+        hidden = true,
+      }
+
       function _lazygit_toggle()
         lazygit:toggle()
       end
 
-      vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+      function _normal_toggle()
+        normalterm:toggle()
+      end
+
+      vim.api.nvim_set_keymap('n', '<leader>tg', '<cmd>lua _lazygit_toggle()<CR>', { desc = 'LazyGit', noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>tn', '<cmd>lua _normal_toggle()<CR>', { desc = 'Terminal', noremap = true, silent = true })
     end,
   },
 }
