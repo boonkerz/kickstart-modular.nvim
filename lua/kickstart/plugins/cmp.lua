27dd125d559b3012e238df2b00fb0cea07342ebe
Thomas <info@thomas-peterson.de>
2025-01-12 21:16:31 +0100

    Fixes

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -14,7 +14,7 @@ return {
         dependencies = {},
       },
       'saadparwaiz1/cmp_luasnip',
-
+      'rafamadriz/friendly-snippets',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
     },
@@ -23,7 +23,7 @@ return {
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
-
+      require('luasnip.loaders.from_vscode').lazy_load()
       cmp.setup {
         snippet = {
           expand = function(args)
@@ -33,8 +33,8 @@ return {
         completion = { completeopt = 'menu,menuone,noinsert' },
 
         mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-y>'] = cmp.mapping.confirm { select = true },
--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -30,6 +30,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        php = { 'phpcbf', 'php_cs_fixer' },
       },
     },
   },
